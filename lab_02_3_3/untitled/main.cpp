a820626c0c81e65110fbb4bbd7989934a3d7de0e
Rudnev Kirill <kirilvasya@gmail.com>
2018-03-14 21:26:36 +0300

    The third task was added.

--- lab_02_3_3/untitled/main.cpp
+++ lab_02_3_3/untitled/main.cpp
@@ -1,8 +1,34 @@
-#include <QCoreApplication>
+#include <stdio.h>
+#include <math.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    float eps, x;
 
-    return a.exec();
+    printf("vvedi eps\n");
+    scanf("%f", &eps);
+
+    printf("vvedi x\n");
+    scanf("%f", &x);
+
+    printf("\n");
+
+    int koef = 1;
+    float t = x/koef;
+    float mnoz = -x*x*koef/(koef + 2);
+    float sum = t;
+    while (t > eps)
+    {
+        t = t*mnoz;
+        sum = sum + t;
+        koef = koef + 2;
+    }
+    printf("zhnachenie funkzii s(x) = %f \n", sum);
+    printf("zhnachenie funkzii f(x) = %f \n", atan(x));
+
+    float absol = fabs(atan(x) - sum);
+    printf("absolutanaya oshibka = %f \n", absol);
+
+    float otnos = fabs(absol/atan(x));
+    printf("otnositelnaya oshibka = %f ", otnos);
 }
