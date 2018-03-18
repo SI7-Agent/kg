3af8d9d6ebc52fd44948845958f641d7303ca405
Rudnev Kirill <kirilvasya@gmail.com>
2018-03-18 17:40:09 +0300

    Changed type of data to float

--- lab_02_3_2/main.c
+++ lab_02_3_2/main.c
@@ -2,53 +2,53 @@
 
 int main()
 {
-    int x, y, k, b, r1, r2, r3, r4;
+    float x, y, k, b, r1, r2, r3, r4;
 
     printf("x\n");
-    r1 = scanf("%d", &x);
+    r1 = scanf("%f", &x);
     printf("\n");
 
     while ((r1 != 1) && (x != 0))
     {
         fflush(stdin);
         printf("Nevernie dannie, povtorite vvod\n");
-        r1 = scanf("%d", &x);
+        r1 = scanf("%f", &x);
     }
 
     printf("y\n");
     fflush(stdin);
-    r2 = scanf("%d", &y);
+    r2 = scanf("%f", &y);
     printf("\n");
 
     while (r2 != 1 && (y != 0))
     {
         fflush(stdin);
         printf("Nevernie dannie, povtorite vvod\n");
-        r2 = scanf("%d", &y);
+        r2 = scanf("%f", &y);
     }
 
     printf("k\n");
     fflush(stdin);
-    r3 = scanf("%d", &k);
+    r3 = scanf("%f", &k);
     printf("\n");
 
     while (r3 != 1 && (k != 0))
     {
         fflush(stdin);
         printf("Nevernie dannie, povtorite vvod\n");
-        r3 = scanf("%d", &k);
+        r3 = scanf("%f", &k);
     }
 
-    printf("b\n");
+    printf("f\n");
     fflush(stdin);
-    r4 = scanf("%d", &b);
+    r4 = scanf("%f", &b);
     printf("\n");
 
     while (r4 != 1 && (b != 0))
     {
         fflush(stdin);
         printf("Nevernie dannie, povtorite vvod\n");
-        r4 = scanf("%d", &b);
+        r4 = scanf("%f", &b);
     }
 
     if (y == k*x + b)
