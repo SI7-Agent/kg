957727a0cafed1440df659bcfd0c44abcccd10bf
Rudnev Kirill <kirilvasya@gmail.com>
2018-09-16 14:33:26 +0300

    added functions

---
+++ lab_07_4/key.c
@@ -0,0 +1,47 @@
+int key(int *array_start, int *array_end, int *array2_start, int *array2_end)
+{
+    int average = 0;
+    int ready_to_filter = 0;
+
+    if (array_end - array_start == 0)
+    {
+        printf ("Incorrect Data");
+        return -1;
+    }
+
+    for (int i = 0; i < array_end - array_start; i++)
+        average = average + *(array_start+i);
+
+    average = average/(array_end - array_start);
+
+    for (int i = 0; i < array_end - array_start; i++)
+        if (*(array_start+i) > average)
+            ready_to_filter++;
+
+    printf("\nReady to filter %d object(-s):\n", ready_to_filter);
+
+    if (ready_to_filter == 0)
+    {
+        printf("No elements to filter\n");
+        return -1;
+    }
+
+    array2_start = (int *)realloc(array2_start, ready_to_filter*sizeof(int));
+    array2_end = array2_start+ready_to_filter;
+
+    int size2 = 0;
+    for (int i = 0; i < array_end - array_start; i++)
+    {
+        if (*(array_start+i) > average)
+        {
+            int temp = *(array_start+i);
+            *(array2_start+size2) = temp;
+            size2++;
+        }
+    }
+
+    for(int b = 0; b < size2; b++)
+        printf("%d\n", *(array2_start+b));
+
+    return ready_to_filter;
+}
---
+++ lab_07_4/mysort.c
@@ -0,0 +1,16 @@
+void mysort(int *array_start, int *array_end)
+{
+    int newElement, location;
+
+    for (int i = 1; i < array_end - array_start; i++)
+    {
+        newElement = *(array_start+i);
+        location = i - 1;
+        while(location >= 0 && *(array_start+location) > newElement)
+        {
+            *(array_start+location+1) = *(array_start+location);
+            location = location - 1;
+        }
+        *(array_start+location+1) = newElement;
+    }
+}
