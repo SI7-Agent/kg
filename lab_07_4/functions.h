f72b73d16847a86ec95f75ed1ae642263e11a993
Rudnev Kirill <kirilvasya@gmail.com>
2018-09-19 20:36:54 +0300

    updated header file

--- lab_07_4/functions.h
+++ lab_07_4/functions.h
@@ -3,5 +3,8 @@
 
 int key(int *array_start, int *array_end, int *array2_start, int *array2_end);
 void mysort(int *array_start, int *array_end);
+void output(int *array_start, int *array_end);
+void record (FILE *f, int *array_start, int *array_end);
+unsigned long long tick(void);
 
 #endif
