67c21dcca045bb48278380632e377accbe013ecc
Rudnev Kirill <kirilvasya@gmail.com>
2018-10-01 09:42:55 +0300

    header file updated

--- lab_07_4/functions.h
+++ lab_07_4/functions.h
@@ -2,9 +2,12 @@
 #define FUNCTION_H
 
 int key(int *array_start, int *array_end, int *array2_start, int *array2_end);
+int get_pos(FILE *f);
+int get_size(FILE *f);
 void mysort(int *array_start, int *array_end);
 void output(int *array_start, int *array_end);
 void record (FILE *f, int *array_start, int *array_end);
+void read_array(FILE *f, int *array_orig);
 unsigned long long tick(void);
 
 #endif
