34e7044a8b48611ada4de7b0a654fafce89defda
Rudnev Kirill <kirilvasya@gmail.com>
2018-10-10 16:26:40 +0300

    useless var fixed

--- lab_07_4/read_array_test.c
+++ lab_07_4/read_array_test.c
@@ -13,7 +13,6 @@ void read_array(FILE *f, int *array_orig, int *array_orig_end)
     int num;
     fseek(f, 0, SEEK_SET);
 
-    int i = 0;
     while(fscanf(f, "%d", &num) == 1)
     {
         *array_orig = num;
