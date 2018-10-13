10fc45b1b7d6bb822ee38089c93356aa0ac80a97
Rudnev Kirill <kirilvasya@gmail.com>
2018-10-13 15:09:17 +0300

    doxygen

--- lab_07_4/key.c
+++ lab_07_4/key.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- Фильтрует указанным способом массив array_start,
- под полученное число элементов создаетс€ массив array2_start,
- в который копируютс€ элементы, удовлетвор€ющие фильтру.
+Фильтрует указанным способом массив array_start,
+под полученное число элементов создаетс€ массив array2_start,
+в который копируютс€ элементы, удовлетвор€ющие фильтру.
 
- * @param array_start
- * @param array_end
- * @param array2_start
- * @param array2_end
+@param array_start
+@param array_end
+@param array2_start
+@param array2_end
 
- * @return возвращает размер отфильтрованного массива
- * или код ошибки в случае некорректных данных
+@return возвращает размер отфильтрованного массива
+или код ошибки в случае некорректных данных
  */
 
 int key(int *array_start, int *array_end, int **array2_start, int **array2_end)
