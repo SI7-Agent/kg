7ecac7b762a877be2b4be26020eb988a0d5d1cdd
Rudnev Kirill <kirilvasya@gmail.com>
2018-10-14 16:58:20 +0300

    updated header file

--- lab_07_4/functions.h
+++ lab_07_4/functions.h
@@ -5,7 +5,7 @@ int key(int *array_start, int *array_end, int **array2_start, int **array2_end);
 int get_pos(FILE *f);
 int get_size(FILE *f);
 int comp_int (const void *i, const void *j);
-void mysort(int *array_start, int *array_end);
+void mysort(void* array_start, void* array_end, size_t size, int (*compar)(const void*, const void*));
 void output(int *array_start, int *array_end);
 void record (FILE *f, int *array_start, int *array_end);
 void read_array(FILE *f, int *array_orig, int *array_orig_end);
