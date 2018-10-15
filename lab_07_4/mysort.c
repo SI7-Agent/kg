cd205b442544b050f796f03664b9ef432d8778f0
Rudnev Kirill <kirilvasya@gmail.com>
2018-10-15 09:52:32 +0300

    updated mysort

--- lab_07_4/mysort.c
+++ lab_07_4/mysort.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- Сортирует по возрастанию массив array_start.
+ Cортирует по возрастанию массив array_start.
 
  * @param array_start
  * @param array_end
@@ -12,14 +12,17 @@
 
 void mysort (void* array_start, void* array_end, size_t size, int (*compar)(const void*, const void*))
 {
-    void *newElement, *member = malloc(size*sizeof(void*)), *location, *base = array_start;
-    array_start += size;
-    for (; array_start < array_end; array_start += size)
+    void *member = malloc(size*sizeof(void*)), *base = array_start;
+	char *newElement, *location;
+	char *start = (char*)array_start;
+	char *end = (char*)array_end;
+    start += size;
+    for (; start < end; start += size)
     {
-        newElement = array_start;
+        newElement = start;
         memcpy(member, newElement, size);
-        location = array_start - size;
-        while(location >= base && compar(location, newElement) > 0)
+        location = start - size;
+        while((void*)location >= base && compar(location, newElement) > 0)
         {
             memcpy(member, newElement, size);
             memcpy(newElement, location, size);
@@ -30,3 +33,17 @@ void mysort (void* array_start, void* array_end, size_t size, int (*compar)(cons
     }
     free(member);
 }
+
+/**
+ Сравнивает два элемента.
+
+ * @param i
+ * @param j
+ * @return возвращает либо число, большее 0, либо меньшее 0, либо 0.
+ */
+
+int comp_int (const void *i, const void *j)
+{
+    return *(int*)i - *(int*)j;
+}
+
