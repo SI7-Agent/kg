3d7325e50b4a6d51a8b46f1ede71331959ac6d19
Rudnev Kirill <kirilvasya@gmail.com>
2018-10-15 09:41:55 +0300

    updated tech func

--- lab_07_4/get_size_test.c
+++ lab_07_4/get_size_test.c
@@ -19,3 +19,24 @@ int get_size(FILE *f)
 
     return size;
 }
+
+/**
+ Узнает местоположение курсора после
+ последнего удачно считанного числа.
+
+ * @param f
+ * @return возвращает местоположение курсора после
+ последнего удачно считанного числа.
+ */
+
+int get_pos(FILE *f)
+{
+    int num;
+    int pos;
+    fseek(f, 0, SEEK_SET);
+
+    while(fscanf(f, "%d", &num) == 1)
+        pos = ftell(f);
+
+    return pos;
+}
--- lab_07_4/read_array_test.c
+++ lab_07_4/read_array_test.c
@@ -20,3 +20,41 @@ void read_array(FILE *f, int *array_orig, int *array_orig_end)
             array_orig++;
     }
 }
+
+/**
+ Выводит на экран массив array_start.
+
+ * @param array_start
+ * @param array_end
+ */
+
+void output(int *array_start, int *array_end)
+{
+    int size = array_end - array_start;
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", *array_start);
+        if (array_start != array_end)
+            array_start++;
+    }
+}
+
+/**
+ Записывает массив array_start в файл f.
+
+ * @param f
+ * @param array_start
+ * @param array_end
+ */
+
+void record (FILE *f, int *array_start, int *array_end)
+{
+    int size = array_end - array_start;
+    for (int i = 0; i < size; i++)
+    {
+        fprintf(f, "%d ", *array_start);
+        if (array_start != array_end)
+            array_start++;
+    }
+}
+
