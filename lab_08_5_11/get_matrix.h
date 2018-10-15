ece0002fa251a02614e8c8ae5cc52801f375edbe
Rudnev Kirill <kirilvasya@gmail.com>
2018-10-15 14:36:18 +0300

    dedicated header file for each .c-file

---
+++ lab_08_5_11/get_matrix.h
@@ -0,0 +1,2 @@
+float **allocate_matrix(int rows, int cols);
+void fill_matrix(FILE *f, float **matr, int rows, int cols);
---
+++ lab_08_5_11/operations.h
@@ -0,0 +1,4 @@
+float **get_sum(float **matr1, float **matr2, int rows, int cols);
+float **get_multy(float **matr1, float **matr2, int rows, int cols, int num1);
+void get_matr(float **matr, float **p, int i, int j, int m);
+float determinant(float **matr, int m, int size);
