7fe3cea3b5f9e24de2f0b0a44537d04ef1f21fec
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-03 14:34:15 +0300

    updated

--- lab_08_5_11/main.c
+++ lab_08_5_11/main.c
@@ -18,32 +18,22 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	
-    int rows1; int cols1;
+    int rows1;
+    int cols1;
 
-    fscanf(matr1, "%d", &rows1);
-    fscanf(matr1, "%d", &cols1);
-
-    float **matrix1 = allocate_matrix(rows1, cols1);
-	
-	if (matrix1 == NULL)
-	{
-		printf("Allocation error\n");
-		return -1;
-	}
-	
-    fill_matrix(matr1, matrix1, rows1, cols1);
+    float **matrix1 = fill_matrix(matr1, &rows1, &cols1);
 
     if ((argc > 4) && (strcmp(argv[1], "a") == 0))
-        make_sum(matr1, matrix1, rows1, cols1, argv);
+        make_sum(matrix1, rows1, cols1, argv);
 
     if ((argc > 4) && (strcmp(argv[1], "m") == 0))
-        make_multy(matr1, matrix1, rows1, cols1, argv);
+        make_multy(matrix1, rows1, cols1, argv);
 
     if ((argc > 3) && (strcmp(argv[1], "o") == 0))
-		make_det(matr1, matrix1, rows1, cols1, argv);
+        make_det(matrix1, rows1, cols1, argv);
   
     if ((argc > 4) && (strcmp(argv[1], "h") == 0))
-        make_info(argv);
+        make_info();
 
     free_matrix(matrix1);
     fclose(matr1);
