11a24911a6fe6b600b032b7a659e62572a94431d
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-11 20:40:01 +0300

    shorted

--- lab_07_4/main.c
+++ lab_07_4/main.c
@@ -8,110 +8,92 @@
 #include "key.h"
 #include "get_size_test.h"
 #include "mysort.h"
-
+#include "do_filter.h"
 
 int main(int argc, char *argv[])
 {
-    unsigned long long tb, te;
-    long pos, pos_end;
-    FILE *f = fopen(argv[1], "r");
-
-    if (f == NULL)
-    {
-        printf("I/O error\nFile is not found\n");
-
-        FILE *f_out_error = fopen(argv[2], "w");
-        fclose(f_out_error);
-        return -1;
-    }
-
-    int size = get_size(f);
-    if (size == 0)
-    {
-        printf("File is empty/contains incorrect data.\n");
-
-        FILE *f_out_error = fopen(argv[2], "w");
-        fclose(f_out_error);
-        return -1;
-    }
-
-    fseek(f, 0, SEEK_END);
-    pos_end = ftell(f);
-    pos = get_pos(f);
-
-    if (pos != pos_end)
-    {
-        printf("File contains incorrect data");
-
-        FILE *f_out_error = fopen(argv[2], "w");
-        fclose(f_out_error);
-        return -1;
-    }
-
-    int *array_orig = (int *)malloc(size*sizeof(int));
-    int *array_orig_end = array_orig+size;
-
-    int *array_start = array_orig;
-
-    fseek(f, 0, SEEK_SET);
-
-    int *array_for_filter = (int *)malloc(size*sizeof(int));
-    int *array_for_filter_end = array_for_filter+size;
-
-    printf("Read array:\n");
-    read_array(f, array_orig, array_orig_end);
-    output(array_orig, array_orig_end);
-    fclose(f);
-
-    int flag = 0;
-    int size2;
-    if ((argc > 3) && (strcmp(argv[3], "f") == 0))
-    {
-        flag = 1;
-        array_orig = array_start;
-        size2 = key(array_orig, array_orig_end, &array_for_filter, &array_for_filter_end);
-
-        if (size2 == -1)
-        {
-            printf("\nError in filter func");
-            return -1;
-        }
-    }
-
-    if (flag == 1)
-    {
-        tb = tick();
-        mysort(array_for_filter, array_for_filter_end, sizeof(int), comp_int);
-        te = tick();
-
-        printf("\nSorted array:\n");
-        output(array_for_filter, array_for_filter_end);
-
-        printf("\nSort time: %llu nsec\n", (te - tb) / 2);
-
-        FILE *f_out = fopen(argv[2], "w");
-        record(f_out, array_for_filter, array_for_filter_end);
-        fclose(f_out);
-    }
+    int code = 0;
+    if (argc < 3)
+        code = -1;
     else
     {
-        array_orig = array_start;
-        tb = tick();
-        mysort(array_orig, array_orig_end, sizeof(int), comp_int);
-        te = tick();
+        FILE *f = fopen(argv[1], "r");
+        if (f == NULL)
+            code = -2;
+        else
+        {
+            int size = get_size(f);
+            if (size == 0)
+                code = -3;
+            else
+            {
+                long pos, pos_end;
+                fseek(f, 0, SEEK_END);
+                pos_end = ftell(f);
+                pos = get_pos(f);
 
-        array_orig = array_start;
-        printf("\nSorted array:\n");
-        output(array_orig, array_orig_end);
+                if (pos != pos_end)
+                    code = -5;
+                else
+                {
+                    int *array_orig = (int *)malloc(size*sizeof(int));
+                    int *array_orig_end = array_orig+size;
+                    int *array_start = array_orig;
 
-        printf("\nSort time: %llu nsec\n", (te - tb) / 2);
+                    fseek(f, 0, SEEK_SET);
 
-        array_orig = array_start;
-        FILE *f_out = fopen(argv[2], "w");
-        record(f_out, array_orig, array_orig_end);
-        fclose(f_out);
+                    int *array_for_filter = (int *)malloc(size*sizeof(int));
+                    int *array_for_filter_end = array_for_filter+size;
+                    if ((array_orig == NULL)||(array_for_filter == NULL))
+                        code = -6;
+                    else
+                    {
+                        printf("Read array:\n");
+                        read_array(f, array_orig, array_orig_end);
+                        output(array_orig, array_orig_end);
+                        work(&code, argv, argc, array_orig, array_orig_end, array_start, array_for_filter, array_for_filter_end);
+
+                        free(array_for_filter);
+                        free(array_orig);
+                    }
+                }
+            }
+            fclose(f);
+        }
     }
+    switch (code)
+    {
+    case 0:
+        break;
 
-    free(array_for_filter);
-    free(array_orig);
+    case -1:
+        printf("Arguments failed\n");
+        break;
+
+    case -2:
+        printf("In_file failed\n");
+        record_empty(argv);
+        break;
+
+    case -3:
+        printf("File is empty/contains incorrect data.\n");
+        record_empty(argv);
+        break;
+
+    case -4:
+        printf("\nError in filter func\n");
+        break;
+
+    case -5:
+        printf("\nFile contains incorrect data\n");
+        record_empty(argv);
+        break;
+
+    case -6:
+        printf("\nAllocation error\n");
+        break;
+
+    default:
+        printf("\n");
+    }
 }
