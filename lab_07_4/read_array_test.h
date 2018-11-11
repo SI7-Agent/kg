8ad9b5ddb0bac217e433da471db99aba81dd6f2a
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-11 20:40:23 +0300

    updated

--- lab_07_4/read_array_test.h
+++ lab_07_4/read_array_test.h
@@ -1,3 +1,4 @@
 void read_array(FILE *f, int *array_orig, int *array_orig_end);
 void output(int *array_start, int *array_end);
 void record (FILE *f, int *array_start, int *array_end);
+void record_empty(char *argv[]);
