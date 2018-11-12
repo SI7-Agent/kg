080d2debbc0bee6bb14ff7cd280ca7b00904c178
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-12 17:16:28 +0300

    added new funcs

--- lab_09_2/my_libstr.c
+++ lab_09_2/my_libstr.c
@@ -6,15 +6,31 @@
 #include "my_libstr.h"
 
 /**
- РЎРѕР·РґР°РµС‚ РєРѕРїРёСЋ СЃС‚СЂРѕРєРё Рё РІС‹РґРµР»СЏРµС‚ РїРѕРґ РЅРµРµ РїР°РјСЏС‚СЊ.
+ Собственная реализация библиотечной функции strlen().
+
+ * @param s
+ * @return
+ */
+
+size_t my_strlen(const char *s)
+{
+    int i = 0;
+    while(s[i])
+        i++;
+
+    return i*sizeof(char);
+}
+
+/**
+ Создает копию строки в новом массиве.
 
  * @param str
- * @return РІРѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РєРѕРїРёСЋ СЃС‚СЂРѕРєРё.
+ * @return
  */
 
 char *strdup(const char *str)
 {
-    int n = strlen(str) + 1;
+    int n = my_strlen(str) + 1;
     char *dup = malloc(n);
     if (dup)
         strcpy(dup, str);
@@ -23,22 +39,22 @@ char *strdup(const char *str)
 }
 
 /**
- РџРµСЂРµРІС‹РґРµР»СЏРµС‚ РїР°РјСЏС‚СЊ РїРѕРґ СЃС‡РёС‚С‹РІР°РµРјСѓСЋ СЃС‚СЂРѕРєСѓ.
+ Перевыделяет память под считываемую строку.
 
  * @param string
  * @param cur_len
  * @param new_len
- * @return РІРѕР·РІСЂР°С‰Р°РµС‚ РєРѕРґ РѕС€РёР±РєРё.
+ * @return возвращает код ошибки.
  */
 
 int realloc_str(char** string, int cur_len, int new_len)
 {
+    int code = NO_ERROR;
     char *temp_string;
     temp_string = malloc(new_len);
 
     if (!temp_string)
-        return MALLOC_ERROR;
-
+        code = MALLOC_ERROR;
     else
     {
         memset(temp_string, '\0', new_len);
@@ -48,17 +64,18 @@ int realloc_str(char** string, int cur_len, int new_len)
             free(*string);
 
         *string = temp_string;
-        return NO_ERROR;
+        code = NO_ERROR;
     }
+    return code;
 }
 
 /**
- РЎС‡РёС‚С‹РІР°РµС‚ СЃС‚СЂРѕРєСѓ РёР· С„Р°Р№Р»Р°.
+ Считывает строку из файла.
 
  * @param lineptr
  * @param n
  * @param stream
- * @return РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‡РёС‚Р°РЅРЅС‹С… Р±Р°Р№С‚.
+ * @return количество считанных байт.
  */
 
 ssize_t getline(char **lineptr, size_t *n, FILE *stream)
@@ -71,8 +88,8 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream)
 
     while(fgets(buf, 10, stream) != NULL)
     {
-        int read = strlen(buf);
-        code_error = realloc_str(&string, current_length , current_length + read + 1);
+        int read = my_strlen(buf);
+        code_error = realloc_str(&string, current_length, current_length + read + 1);
 
         if (code_error != NO_ERROR)
             break;
@@ -90,19 +107,19 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream)
 }
 
 /**
- Р—Р°РјРµРЅСЏРµС‚ РїРµСЂРІРѕРµ РІС…РѕР¶РґРµРЅРёРµ СѓРєР°Р·Р°РЅРЅРѕР№ РїРѕРґСЃС‚СЂРѕРєРё РІ РґР°РЅРЅРѕР№ СЃС‚СЂРѕРєРµ.
+ Заменяет первое вхождение указанной подстроки в данной строке.
 
  * @param string
  * @param search
  * @param replace
- * @return РІРѕР·РІСЂР°С‰Р°РµС‚ РєРѕРґ РѕС€РёР±РєРё.
+ * @return возвращает код ошибки.
  */
 
 int replace(char **string, const char *search, const char *replace)
 {
-    int size_line = strlen(*string);
-    int size_search = strlen(search);
-    int size_replace = strlen(replace);
+    int size_line = my_strlen(*string);
+    int size_search = my_strlen(search);
+    int size_replace = my_strlen(replace);
 
     if (strstr(*string, search) == NULL)
         return BAD_LENGHT;
@@ -131,12 +148,12 @@ int replace(char **string, const char *search, const char *replace)
 }
 
 /**
- Р—Р°РјРµРЅСЏРµС‚ РІСЃРµ РІС…РѕР¶РґРµРЅРёСЏ СѓРєР°Р·Р°РЅРЅРѕР№ РїРѕРґСЃС‚СЂРѕРєРё РІ РґР°РЅРЅРѕР№ СЃС‚СЂРѕРєРµ.
+ Заменяет все вхождения указанной подстроки в данной строке.
 
  * @param source
  * @param search
  * @param replacement
- * @return РІРѕР·РІСЂР°С‰Р°РµС‚ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РёР·РјРµРЅРµРЅРЅСѓСЋ СЃС‚СЂРѕРєСѓ.
+ * @return возвращает указатель на измененную строку.
  */
 
 char* str_replace(const char *source, const char *search, const char *replacement)
@@ -149,4 +166,72 @@ char* str_replace(const char *source, const char *search, const char *replacemen
         printf("Allocation failed\n");
 
     return linetmp;
+}
+
+/**
+ Заменяет все вхождения указанной подстроки во всех строках входного файла и записывает результат в выходной файл.
+
+ * @param file_in
+ * @param file_out
+ * @param search
+ * @param replace
+ * @return возвращает код ошибки.
+ */
+
+int replace_strings_in_file(FILE *file_in, FILE *file_out, const char *search, const char *replace)
+{
+    char *string = NULL;
+    char *newstring = NULL;
+    int code_error = 0;
+    size_t length = 0;
+
+    while((code_error = getline(&string, &length, file_in) != MALLOC_ERROR) && (string != NULL))
+    {
+        newstring = str_replace(string, search, replace);
+        fprintf(file_out, "%s", newstring);
+
+        if(newstring != NULL)
+            free(newstring);
+
+        if(string != NULL)
+            free(string);
+
+        length = 0;
+    }
+
+    return code_error;
+}
+
+/**
+ Выводит тип ошибки, если таковая имеется, в случае, предусмотренном программой.
+
+ * @param code_error
+ */
+
+void get_error(int code_error)
+{
+    switch(code_error)
+    {
+        case ARGV_ERROR:
+            printf("Arguments failed\n");
+            break;
+
+        case IN_FILE_ERROR:
+            printf("In_file failed\n");
+            break;
+
+        case OUT_FILE_ERROR:
+            printf("Out_file failed\n");
+            break;
+
+        case MALLOC_ERROR:
+            printf("Allocation failed\n");
+            break;
+
+        case NO_ERROR:
+            break;
+
+        default:
+            printf("\n");
+    }
 }
--- lab_09_2/my_libstr.h
+++ lab_09_2/my_libstr.h
@@ -1,12 +1,32 @@
+#ifndef NO_ERROR
 #define NO_ERROR 0
+#endif
+
+#ifndef MALLOC_ERROR
 #define MALLOC_ERROR -1
+#endif
+
+#ifndef ARGV_ERROR
 #define ARGV_ERROR -2
+#endif
+
+#ifndef IN_FILE_ERROR
 #define IN_FILE_ERROR -3
+#endif
+
+#ifndef OUT_FILE_ERROR
 #define OUT_FILE_ERROR -4
+#endif
+
+#ifndef BAD_LENGHT
 #define BAD_LENGHT -5
+#endif
 
 int realloc_str(char** string, int cur_len, int new_len);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 int replace(char **string, const char *search, const char *replace);
 char* str_replace(const char *source, const char *search, const char *replacement);
 char *strdup(const char *str);
+int replace_strings_in_file(FILE *file_in, FILE *file_out, const char *search, const char *replace);
+void get_error(int code_error);
+size_t my_strnlen(const char *s);
