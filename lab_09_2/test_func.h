faef3b679520897d8e92000a6fbc7269ff9b2b31
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-12 17:12:09 +0300

    fixed

---
+++ lab_09_2/test_func.h
@@ -0,0 +1,11 @@
+#ifndef OK
+#define OK 0
+#endif
+
+#ifndef FAIL
+#define FAIL -1
+#endif
+
+char *answer(int val);
+int test_getline(const char *file_name);
+int test_replace(const char *line, const char *replace_what, const char *replace_with, char *answer);
