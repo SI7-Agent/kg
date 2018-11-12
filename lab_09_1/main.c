ead32a7c0af41b196806c19aa9154ac70aa883b5
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-12 17:15:57 +0300

    fixed

--- lab_09_1/main.c
+++ lab_09_1/main.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
-int my_strcspn(char *string, char *key)
-{
-    for (int i = 0; i<sizeof(string)+1; i++)
-    {
-        for (int j = 0; j<sizeof(key); j++)
-        {
-            if (string[i] == key[j])
-            {
-                if ((string[i] == "\0") && (key[j] == "\0"))
-                    return 0;
-                else
-                    return i;
-            }
-        }
-    }
-    return 0;
-}
+#include "my_strcspn.h"
 
 int main()
 {
