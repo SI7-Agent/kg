5bbe7c461b98a80d545c86edc2fd5df4b4a6886e
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-12 17:16:07 +0300

    fixed

--- lab_09_2/main.c
+++ lab_09_2/main.c
@@ -5,40 +5,6 @@
 
 #include "my_libstr.h"
 
-/**
- Заменяет все вхождения указанной подстроки во всех строках входного файла и записывает результат в выходной файл.
-
- * @param file_in
- * @param file_out
- * @param search
- * @param replace
- * @return возвращает код ошибки.
- */
-
-int work(FILE *file_in, FILE *file_out, const char *search, const char *replace)
-{
-    char *string = NULL;
-    char *newstring = NULL;
-    int code_error = 0;
-    size_t length = 0;
-
-    while((code_error = getline(&string, &length, file_in) != MALLOC_ERROR) && (string != NULL))
-    {
-        newstring = str_replace(string, search, replace);
-        fprintf(file_out, "%s", newstring);
-
-        if(newstring != NULL)
-            free(newstring);
-
-        if(string != NULL)
-            free(string);
-
-        length = 0;
-    }
-
-    return code_error;
-}
-
 int main(int argc, char *argv[])
 {
     int code_error = NO_ERROR;
@@ -57,36 +23,13 @@ int main(int argc, char *argv[])
             if (!file_out)
                 code_error = OUT_FILE_ERROR;
             else
-                work(file_in, file_out, argv[3], argv[4]);
+                replace_strings_in_file(file_in, file_out, argv[3], argv[4]);
 
             fclose(file_out);
         }
         fclose(file_in);
     }
 
-    switch(code_error)
-    {
-        case ARGV_ERROR:
-            printf("Arguments failed\n");
-            break;
-
-        case IN_FILE_ERROR:
-            printf("In_file failed\n");
-            break;
-
-        case OUT_FILE_ERROR:
-            printf("Out_file failed\n");
-            break;
-
-        case MALLOC_ERROR:
-            printf("Allocation failed\n");
-            break;
-
-        case NO_ERROR:
-            break;
-
-        default:
-            printf("All's BAD\n");
-    }
+    get_error(code_error);
     return 0;
 }
