12cb479c7273f27404d3a54d141fe6b18823ae08
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-18 20:10:56 +0300

    fixed names and ifndef

--- lab_09_2/my_libstr.h
+++ lab_09_2/my_libstr.h
@@ -1,26 +1,10 @@
-#ifndef NO_ERROR
+#ifndef MY_LIBSTR_H
 #define NO_ERROR 0
-#endif
-
-#ifndef MALLOC_ERROR
 #define MALLOC_ERROR -1
-#endif
-
-#ifndef ARGV_ERROR
 #define ARGV_ERROR -2
-#endif
-
-#ifndef IN_FILE_ERROR
 #define IN_FILE_ERROR -3
-#endif
-
-#ifndef OUT_FILE_ERROR
 #define OUT_FILE_ERROR -4
-#endif
-
-#ifndef BAD_LENGHT
 #define BAD_LENGHT -5
-#endif
 
 int realloc_str(char** string, int cur_len, int new_len);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
@@ -30,3 +14,4 @@ char *strdup(const char *str);
 int replace_strings_in_file(FILE *file_in, FILE *file_out, const char *search, const char *replace);
 void get_error(int code_error);
 size_t my_strnlen(const char *s);
+#endif
