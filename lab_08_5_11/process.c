9651378903db03b0e9db17a6e45e8efdff8396b7
Rudnev Kirill <kirilvasya@gmail.com>
2018-11-18 19:57:34 +0300

    added new func

--- lab_08_5_11/process.c
+++ lab_08_5_11/process.c
@@ -8,7 +8,7 @@
 #include "operations.h"
 
 /**
- Р’С‹РїРѕР»РЅСЏРµС‚ РѕРїРµСЂР°С†РёСЋ СЃР»РѕР¶РµРЅРёСЏ РјР°С‚СЂРёС† СЃРѕ РІСЃРµРјРё РїСЂРѕРІРµСЂРєР°РјРё РѕС‚ РїРѕР»СѓС‡РµРЅРёСЏ С„Р°Р№Р»Р° РІС‚РѕСЂРѕР№ РјР°С‚СЂРёС†С‹ РґРѕ РїРµС‡Р°С‚Рё СЂРµР·СѓР»СЊС‚Р°С‚Р° РІ С„Р°Р№Р».
+ Выполняет операцию сложения матриц со всеми проверками от получения файла второй матрицы до печати результата в файл.
 
  * @param matrix1
  * @param rows1
@@ -16,32 +16,49 @@
  * @param argv
  */
 
-void make_sum(float **matrix1, float **matrix2, int rows1, int cols1, int rows2, int cols2, char *argv[])
+void make_sum(float **matrix1, int rows1, int cols1, char *argv[])
 {
-    output(matrix1, rows1, cols1);
-    printf("\n");
-    output(matrix2, rows2, cols2);
-
-    if ((rows1 != rows2) || (cols1 != cols2))
-        printf("\nERROR! Wrong sizes of matrixes\n");
+    FILE *matr2 = fopen(argv[3], "r");
+    if (!matr2)
+        printf("Error opening file\n");
     else
     {
-        float **res = get_sum(matrix1, matrix2, rows1, cols1);
+        int rows2;
+        int cols2;
 
-        if (res == NULL)
-            printf("\nResult allocation error\n");
-        else
+        float **matrix2 = fill_matrix(matr2, &rows2, &cols2);
+        if (matrix2 != NULL)
         {
+            output(matrix1, rows1, cols1);
             printf("\n");
-            output(res, rows1, cols1);
-            record_matr(argv, res, rows1, cols1);
-            free_matrix(res);
+            output(matrix2, rows2, cols2);
+
+            if ((rows1 != rows2) || (cols1 != cols2))
+                printf("\nERROR! Wrong sizes of matrixes\n");
+            else
+            {
+                float **res = get_sum(matrix1, matrix2, rows1, cols1);
+
+                if (res == NULL)
+                    printf("\nResult allocation error\n");
+                else
+                {
+                    printf("\n");
+                    output(res, rows1, cols1);
+                    record_matr(argv, res, rows1, cols1);
+                    free_matrix(res);
+                }
+            }
+            free_matrix(matrix2);
         }
+        else
+            printf("Allocation error\n");
+        fclose(matr2);
     }
 }
 
 /**
- Р’С‹РїРѕР»РЅСЏРµС‚ РѕРїРµСЂР°С†РёСЋ СѓРјРЅРѕР¶РµРЅРёСЏ РјР°С‚СЂРёС† СЃРѕ РІСЃРµРјРё РїСЂРѕРІРµСЂРєР°РјРё РѕС‚ РїРѕР»СѓС‡РµРЅРёСЏ С„Р°Р№Р»Р° РІС‚РѕСЂРѕР№ РјР°С‚СЂРёС†С‹ РґРѕ РїРµС‡Р°С‚Рё СЂРµР·СѓР»СЊС‚Р°С‚Р° РІ С„Р°Р№Р».
+ Выполняет операцию умножения матриц со всеми проверками от получения файла второй матрицы до печати результата в файл.
 
  * @param matrix1
  * @param rows1
@@ -49,32 +66,49 @@ void make_sum(float **matrix1, float **matrix2, int rows1, int cols1, int rows2,
  * @param argv
  */
 
-void make_multy(float **matrix1, float **matrix2, int rows1, int cols1, int rows2, int cols2, char *argv[])
+void make_multy(float **matrix1, int rows1, int cols1, char *argv[])
 {
-    output(matrix1, rows1, cols1);
-    printf("\n");
-    output(matrix2, rows2, cols2);
-
-    if (cols1 != rows2)
-        printf("\nERROR! Wrong sizes of matrixes\n");
+    FILE *matr2 = fopen(argv[3], "r");
+    if (!matr2)
+        printf("Error opening file\n");
     else
     {
-        float **res2 = get_multy(matrix1, matrix2, rows1, cols2, rows2);
+        int rows2;
+        int cols2;
 
-        if (res2 == NULL)
-            printf("\nResult allocation error\n");
-        else
+        float **matrix2 = fill_matrix(matr2, &rows2, &cols2);
+        if (matrix2 != NULL)
         {
+            output(matrix1, rows1, cols1);
             printf("\n");
-            output(res2, rows1, cols2);
-            record_matr(argv, res2, rows1, cols2);
-            free_matrix(res2);
+            output(matrix2, rows2, cols2);
+
+            if (cols1 != rows2)
+                printf("\nERROR! Wrong sizes of matrixes\n");
+            else
+            {
+                float **res2 = get_multy(matrix1, matrix2, rows1, cols2, rows2);
+
+                if (res2 == NULL)
+                    printf("\nResult allocation error\n");
+                else
+                {
+                    printf("\n");
+                    output(res2, rows1, cols2);
+                    record_matr(argv, res2, rows1, cols2);
+                    free_matrix(res2);
+                }
+            }
+            free_matrix(matrix2);
         }
+        else
+            printf("Allocation error\n");
+        fclose(matr2);
     }
 }
 
 /**
- Р’С‹РїРѕР»РЅСЏРµС‚ РѕРїРµСЂР°С†РёСЋ РІС‹С‡РёСЃР»РµРЅРёСЏ РґРµС‚РµСЂРјРёРЅР°РЅС‚Р° СЃРѕ РІСЃРµРјРё РїСЂРѕРІРµСЂРєР°РјРё РґРѕ РїРµС‡Р°С‚Рё СЂРµР·СѓР»СЊС‚Р°С‚Р° РІ С„Р°Р№Р».
+ Выполняет операцию вычисления детерминанта со всеми проверками до печати результата в файл.
 
  * @param matrix1
  * @param rows1
@@ -99,7 +133,7 @@ void make_det(float **matrix1, int rows1, int cols1, char *argv[])
 }
 
 /**
- Р’С‹РїРѕР»РЅСЏРµС‚ РѕРїРµСЂР°С†РёСЋ РІС‹РІРѕРґР° СЃРїСЂР°РІРѕС‡РЅРѕР№ РёРЅС„РѕСЂРјР°С†РёРё.
+ Выполняет операцию вывода справочной информации.
 
  */
 
@@ -118,5 +152,40 @@ void make_info()
             printf("%s", string);
         }
         fclose(f_info);
+    }
+}
+
+void process(char *argv[])
+{
+    FILE *matr1 = fopen(argv[2], "r");
+
+    if (!matr1)
+        printf("Error opening file\n");
+    else
+    {
+        int rows1;
+        int cols1;
+
+        float **matrix1 = fill_matrix(matr1, &rows1, &cols1);
+        if (matrix1 != NULL)
+        {
+            if (strcmp(argv[1], "a") == 0)
+                make_sum(matrix1, rows1, cols1, argv);
+
+            if (strcmp(argv[1], "m") == 0)
+                make_multy(matrix1, rows1, cols1, argv);
+
+            if (strcmp(argv[1], "o") == 0)
+                make_det(matrix1, rows1, cols1, argv);
+
+            if (strcmp(argv[1], "h") == 0)
+                make_info();
+
+            free_matrix(matrix1);
+        }
+        else
+            printf("Allocation error\n");
+
+        fclose(matr1);
     }
 }
