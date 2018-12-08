a925ca559029ae90fc3532de4df015f8e0b7bf08
Rudnev Kirill <kirilvasya@gmail.com>
2018-12-08 18:22:52 +0300

    added main programm

---
+++ lab_11/main.c
@@ -0,0 +1,27 @@
+#include "func.h"
+#include "test.h"
+
+int main()
+{
+    int test_counter = 1;
+    char string1[25], string2[25];
+    {
+        const char *command = "hex %hx";
+        int buff_s = 25;
+        int data_array[] = {-65431, -32769, -32768, 100, 32766, 32768, 654321, -100, 0, 32767};
+        for (int i = 0; i < 10; ++i)
+        {
+            int n = data_array[i];
+            int read_byte1 = snprintf(string1, buff_s, command, n);
+            int read_byte2 = my_snprintf(string2, buff_s, command, n);
+            answer(compare(string1, string2, read_byte1, read_byte2), test_counter);
+            test_counter++;
+        }
+    }
+
+    answer(test1(), test_counter);
+    test_counter++;
+    answer(test2(), test_counter);
+
+    return 0;
+}
