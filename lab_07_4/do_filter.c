ac6babc981b5049f4da4a3f2dade4c7cee3d6dc1
Rudnev Kirill <kirilvasya@gmail.com>
2018-12-10 17:29:58 +0300

    fixed mem checks

--- lab_07_4/do_filter.c
+++ lab_07_4/do_filter.c
@@ -22,15 +22,10 @@
 
 void sort_with_filt(int *array_for_filter, int *array_for_filter_end, char *argv[])
 {
-    unsigned long long tb, te;
-    tb = tick();
     mysort(array_for_filter, array_for_filter_end, sizeof(int), comp_int);
-    te = tick();
 
     printf("\nSorted array:\n");
     output(array_for_filter, array_for_filter_end);
-
-    printf("\nSort time: %llu nsec\n", (te - tb) / CLOCKS_PER_SEC);
 
     FILE *f_out = fopen(argv[2], "w");
     record(f_out, array_for_filter, array_for_filter_end);
@@ -48,17 +43,12 @@ void sort_with_filt(int *array_for_filter, int *array_for_filter_end, char *argv
 
 void sort_with_no_filt(int *array_orig, int *array_orig_end,int *array_start, char *argv[])
 {
-    unsigned long long tb, te;
     array_orig = array_start;
-    tb = tick();
     mysort(array_orig, array_orig_end, sizeof(int), comp_int);
-    te = tick();
 
     array_orig = array_start;
     printf("\nSorted array:\n");
     output(array_orig, array_orig_end);
-
-    printf("\nSort time: %llu nsec\n", (te - tb) / CLOCKS_PER_SEC);
 
     array_orig = array_start;
     FILE *f_out = fopen(argv[2], "w");
@@ -89,11 +79,13 @@ void work(int *code, char *argv[], int argc, int *array_orig, int *array_orig_en
         array_orig = array_start;
         size2 = key(array_orig, array_orig_end, &array_for_filter, &array_for_filter_end);
 
-        if (size2 < 0)
+        if (!array_for_filter)
+			*code = mem_error;
+        else if (size2 < 0)
             *code = bad_filter;
     }
 
-    if ((flag == 1) && (size2 > 0))
+    if ((flag == 1) && (size2 > 0) && (array_for_filter))
         sort_with_filt(array_for_filter, array_for_filter_end, argv);
     else if ((argc > 2) && (flag == 0))
         sort_with_no_filt(array_orig, array_orig_end, array_start, argv);
