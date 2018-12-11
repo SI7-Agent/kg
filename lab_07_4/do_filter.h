20fb490a40163880f3952b0e996021f4706ecffc
Rudnev Kirill <kirilvasya@gmail.com>
2018-12-11 21:58:03 +0300

    updated headers

--- lab_07_4/do_filter.h
+++ lab_07_4/do_filter.h
@@ -1,8 +1,6 @@
 #ifndef DO_FILTER_H
 #define DO_FILTER_H
 
-void sort_with_no_filt(int *array_orig, int *array_orig_end,int *array_start, char *argv[]);
-void sort_with_filt(int *array_for_filter, int *array_for_filter_end, char *argv[]);
-void work(int *code, char *argv[], int argc, int *array_orig, int *array_orig_end, int *array_start, int *array_for_filter, int *array_for_filter_end);
+void work(int *code, char *argv[], int argc, int *array_orig, int *array_orig_end, int *array_for_filter, int *array_for_filter_end);
 
 #endif
