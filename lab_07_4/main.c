3728656e84f3a6a468d15a809d6095c0ff8df835
Rudnev Kirill <kirilvasya@gmail.com>
2018-12-13 17:55:36 +0300

    rewrote

--- lab_07_4/main.c
+++ lab_07_4/main.c
@@ -14,49 +14,38 @@
 int main(int argc, char *argv[])
 {
     index code = ok;
+    FILE *f = NULL;
+    int *array_orig = NULL;
+    int *array_orig_end = NULL;
+    int *array_for_filter = NULL;
+    int *array_for_filter_end = NULL;
+
     if (argc < 3)
         code = wrong_argv;
-    else
+
+    if (!code)
     {
-        FILE *f = fopen(argv[1], "r");
-        if (!f)
-            code = no_file;
-        else
-        {
-            int size = get_size(f);
-            if (size == 0)
-                code = empty_file;
-            else
-            {
-                long pos, pos_end;
-                fseek(f, 0, SEEK_END);
-                pos_end = ftell(f);
-                pos = get_pos(f);
-
-                if (pos != pos_end)
-                    code = bad_data_file;
-                else
-                {
-                    int *array_orig = read_array(f, size);
-                    if (!array_orig)
-                        code = mem_error;
-                    else
-                    {
-                        int *array_orig_end = array_orig + size;
-                        int *array_for_filter = NULL;
-                        int *array_for_filter_end = NULL;
-						
-                        work(&code, argv, argc, array_orig, array_orig_end, array_for_filter, array_for_filter_end);
-
-                        if (array_for_filter)
-                            free(array_for_filter);
-                        if (array_orig)
-                            free(array_orig);
-                    }
-                }
-            }
-            fclose(f);
-        }
+        f = fopen(argv[1], "r");
+        code = check_file(f);
     }
+
+    if (!code)
+    {
+        int size = get_size(f);
+        array_orig = read_array(f, size);
+        array_orig_end = array_orig + size;
+        if (!array_orig)
+            code = mem_error;
+    }
+
+    if (!code)
+        code = work(argv, argc, array_orig, array_orig_end, array_for_filter, array_for_filter_end);
+
+    if (array_orig)
+        free(array_orig);
+
+    if (f)
+        fclose(f);
+
     print_error(code, argv);
 }
