88cd7649adba2bf66ed075dd6846885e4bab39f5
Rudnev Kirill <kirilvasya@gmail.com>
2018-12-16 18:01:06 +0300

    updated tests

--- lab_07_4/test.c
+++ lab_07_4/test.c
@@ -97,7 +97,7 @@ int main()
 	
     unsigned long long tb, te;
     tb = tick();
-    mysort(array_orig5, array_orig_end5, sizeof(int), comp_int);
+    mysort(array_orig5, array_orig_end5 - array_orig5, sizeof(int), comp_int);
     te = tick();
     printf("Mysort time is: %d\n", (te - tb));
 	
@@ -115,5 +115,65 @@ int main()
     printf("Qsort average time is: %d nsec\n", middle_t);
     fclose(f5);
     printf("Test passed\n\n");
+	
+    //-----------------------------
+	
+    printf("Testing key func \n");
+    FILE *f6 = fopen("in_5.txt", "r");
+    int size6 = get_size(f6);
+    int *array_orig6 = read_array(f6, size6);
+    int *array_orig_end6 = array_orig6 + size6;
+
+    int *array_for_filter6 = NULL;
+    int *array_for_filter_end6 = NULL;
+
+    key(array_orig6, array_orig_end6, &array_for_filter6, &array_for_filter_end6);
+    FILE *f6o = fopen("out_5.txt", "r");
+    int flag = 1;
+    int var_fr_file;
+    for (; (array_for_filter6 != array_for_filter_end6) && (flag); array_for_filter6++)
+    {
+        fscanf(f6o, "%d", &var_fr_file);
+        if (*array_for_filter6 != var_fr_file)
+            flag = 0;
+    }
+    if (flag)
+        printf("Test passed\n\n");
+    else
+    {
+        printf("Test failed\n\n");
+    }
+	
+    free(array_orig6);
+    free(array_for_filter6);
+    fclose(f6);
+    fclose(f6o);
+	
+    //------------------------------
+	
+    printf("Testing mysort func \n");
+    FILE *f7 = fopen("in_6.txt", "r");
+    int size7 = get_size(f7);
+    int *array_orig7 = read_array(f7, size7);
+    int *array_orig_end7 = array_orig7 + size7;
+
+    mysort(array_orig7, size7, sizeof(int), comp_int);
+    FILE *f7o = fopen("out_6.txt", "r");
+    flag = 1;
+    for (; (array_orig7 != array_orig_end7) && (flag); array_orig7++)
+    {
+        fscanf(f7o, "%d", &var_fr_file);
+        if (*array_orig7 != var_fr_file)
+            flag = 0;
+    }
+    if (flag)
+        printf("Test passed\n\n");
+    else
+        printf("Test failed\n\n");
+	
+    free(array_orig7);
+    fclose(f7);
+    fclose(f7o);
+	
     return 0;
 }
